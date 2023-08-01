480e41ad61a8b7863b7be7c373b6b55e56b59b04
wac <wababam9@gmail.com>
2023-08-01 04:30:52 -0700

    Add layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import './style.css';
-import logo from './images/naver 1.png';
 
-const lg = document.querySelector('.lg');
+const ul = document.getElementById('ul');
+const form = document.getElementById('frm-add');
 
-lg.src = logo;
-lg.alt = 'Logo';
+const scores = [
+  { name: 'John', score: 80 },
+  { name: 'Mathew', score: 100 },
+  { name: 'Wonde', score: 120 },
+  { name: 'Solomon', score: 50 },
+  { name: 'Rose', score: 75 },
+  { name: 'Lily', score: 90 },
+  { name: 'Wac', score: 130 },
+];
+const populareScoreBoard = () => {
+  if (scores !== []) {
+    ul.innerHTML = '';
+    scores.forEach((score) => {
+      const li = document.createElement('li');
+      li.innerHTML = `${score.name}: ${score.score}`;
+      ul.appendChild(li);
+    });
+  }
+};
+
+const addSubmitListener = () => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = form.elements.name.value;
+    const score = form.elements.score.value;
+    scores.push({ name, score });
+    populareScoreBoard();
+  });
+};
+
+addSubmitListener();
+populareScoreBoard();
