8e28449354e25bf636e5408e7db1e4597dc504ee
wac <wababam9@gmail.com>
2023-08-02 02:56:51 -0700

    Add API loader

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
 import './style.css';
+import DOMManipulator from './modules/dom_manipulator.js';
 
-const ul = document.getElementById('ul');
-const form = document.getElementById('frm-add');
-
-const scores = [
-  { name: 'John', score: 80 },
-  { name: 'Mathew', score: 100 },
-  { name: 'Wonde', score: 120 },
-  { name: 'Solomon', score: 50 },
-  { name: 'Rose', score: 75 },
-  { name: 'Lily', score: 90 },
-  { name: 'Wac', score: 130 },
-];
-const populareScoreBoard = () => {
-  if (scores !== []) {
-    ul.innerHTML = '';
-    scores.forEach((score) => {
-      const li = document.createElement('li');
-      li.innerHTML = `${score.name}: ${score.score}`;
-      ul.appendChild(li);
-    });
-  }
-};
-
-const addSubmitListener = () => {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const name = form.elements.name.value;
-    const score = form.elements.score.value;
-    scores.push({ name, score });
-    populareScoreBoard();
-  });
-};
-
-addSubmitListener();
-populareScoreBoard();
+const dom = new DOMManipulator();
+dom.addSubmitListener();
+dom.populateScoreBoard();
+dom.addRefreshListener();
