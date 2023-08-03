e134f495dd93f84504eccb64984ada6db654890c
wac <wababam9@gmail.com>
2023-08-03 02:42:22 -0700

    Make final touches

--- src/modules/dom_manipulator.js
+++ src/modules/dom_manipulator.js
@@ -1,4 +1,5 @@
 import loader from './api_loader.js';
+import crown from '../images/crown.png';
 
 const ul = document.getElementById('ul');
 const form = document.getElementById('frm-add');
@@ -7,12 +8,30 @@ const info = document.querySelector('.info');
 
 class DOMManipulator {
   populateScoreBoard = async () => {
-    const scores = await loader.getData();
+    let scores = await loader.getData();
     if (scores.result !== []) {
       ul.innerHTML = '';
-      scores.result.forEach((score) => {
+      scores = scores.result.sort((a, b) => b.score - a.score);
+      scores.forEach((score, idx) => {
+        let crImg = null;
         const li = document.createElement('li');
-        li.innerHTML = `${score.user}: ${score.score}`;
+        const table = document.createElement('table');
+        const row = table.insertRow();
+        const col1 = row.insertCell();
+        const col2 = row.insertCell();
+        const col3 = row.insertCell();
+        col1.innerHTML = score.user;
+        col2.innerHTML = score.score;
+        li.appendChild(table);
+        if (idx === 0) {
+          crImg = document.createElement('img');
+          crImg.classList.add('crown');
+          col3.appendChild(crImg);
+          crImg.src = crown;
+          li.style.color = 'green';
+          li.style.fontSize = '15px';
+          li.style.fontWeight = '800';
+        }
         ul.appendChild(li);
       });
     }
@@ -23,15 +42,19 @@ class DOMManipulator {
       event.preventDefault();
       const name = form.elements.name.value;
       const score = form.elements.score.value;
-      const result = loader.setData({ user: name, score });
-      let id = null;
-      let msg = null;
-      result.then((res) => {
-        id = res.id;
-        msg = res.message;
+      if (name && score) {
+        const result = loader.setData({ user: name, score });
+        let id = null;
+        let msg = null;
+        result.then((res) => {
+          id = res.id;
+          msg = res.message;
+          this.showNotification(msg, id);
+        });
         this.showNotification(msg, id);
-      });
-      this.showNotification(msg, id);
+      } else {
+        this.showNotification('Name or Score field is empty!', 0);
+      }
     });
   };
 
